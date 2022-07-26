81b3b1740b8d0cdcba5ef5fa48ae10f6c1a84800
Madan <madanmohan7272906844@gmail.com>
2022-07-26 11:16:44 +0530

    updated

--- index.js
+++ index.js
@@ -38,6 +38,45 @@ function finishTodo(title1,dueDate1,id1){
   saveCompleteTodos();
 }
 
+function setEditing(todoID){
+  todos.forEach((todo)=>{
+    if(todo.id == todoID){
+      todo.isEditing = true;
+    }
+  });
+}
+
+function onEdit(event){
+  const editButton = event.target;
+  const todoID = editButton.dataset.todoID;
+  
+  setEditing(todoID);
+  render();
+}
+
+function updateTodo(todoID,newTitle,newdueDate){
+  todos.forEach((todo)=>{
+    if(todo.id === todoID){
+      todo.title = newTitle;
+      todo.dueDate = newdueDate;
+      todo.isEditing = false;
+    }
+  });
+  saveTodos();
+}
+function onUpdate(event){
+  const updateButton = event.target;
+  const todoID = updateButton.dataset.todoID;
+
+  const textBox = document.getElementById('edit-title-' + todoID);
+  const newTitle = textBox.value;
+
+  const DatePicker = document.getElementById('edit-date-' + todoID);
+  const newdueDate = DatePicker.value;
+
+  updateTodo(todoID,newTitle,newdueDate);
+  render();
+}
 function add_todo(){
 
   const textBox = document.getElementById('todo-title');
@@ -136,8 +175,37 @@ function render(){
     element.className = 'elementClass';
     element.innerText = todo.title + ' ' + todo.dueDate;
 
+    if(todo.isEditing === true){
+      const textBox = document.createElement('input');
+      textBox.type = 'text';
+      textBox.id = 'edit-title-' + todo.id;
+      textBox.className = 'update-textbox'
+      element.appendChild(textBox);
+
+      const DatePicker = document.createElement('input');
+      DatePicker.type = 'datetime-local';
+      DatePicker.id = 'edit-date-' + todo.id;
+      DatePicker.className = 'update-date';
+      element.appendChild(DatePicker);
+
+      const updateButton = document.createElement('button');
+      updateButton.innerText = 'update';
+      updateButton.dataset.todoID = todo.id;
+      updateButton.onclick = onUpdate;
+      updateButton.className = 'update-btn';
+      element.appendChild(updateButton);
+    }
+    
+    const editButton = document.createElement('button');
+    editButton.innerText = 'Edit';
+    editButton.style = 'margin-left: 12px';
+    editButton.onclick = onEdit;
+    editButton.dataset.todoID = todo.id;
+    editButton.className = 'edit-btn';
+    element.appendChild(editButton);
+
     let delete_Button = document.createElement('button')
-    delete_Button.innerText = 'x';
+    delete_Button.innerText = 'delete';
     // delete_Button.style = 'margin-left: 10px; border-radius: 8px;';
     delete_Button.className = 'delete';
     delete_Button.onclick = deleteTodo;
@@ -168,7 +236,7 @@ function rerender(){
     element1.innerText = todo.title + ' ' + todo.dueDate;
 
     let delete_Button1 = document.createElement('button')
-    delete_Button1.innerText = 'X';
+    delete_Button1.innerText = 'delete';
     delete_Button1.style = 'margin-left: 10px';
     delete_Button1.onclick = deleteFinish;
     delete_Button1.id = todo.id;
